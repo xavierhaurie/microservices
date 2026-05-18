8264c72a7a752d6ad6675e47c8d585c22f501e1d
xavier_haurie <xavier.haurie@gmail.com>
2026-05-18 11:20:34 -0400

    make API gateway work without naming server -- using RouteLocator

---
+++ 12.api-gateway/src/main/java/com/in28minutes/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,31 @@
+package com.in28minutes.microservices.api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    // instead of using the naming server, we can hard-code the routes
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(r -> r.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(r -> r.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(r -> r.path("/currency-conversion-feign/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-feign/(?<segment>.*)",
+                                "/currency-conversion/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
