2fa9d7b5ac2f5e96bda061bad47a847746172261
xavier_haurie <xavier.haurie@gmail.com>
2026-05-18 11:36:29 -0400

    minor clean up

--- 6.limits-service/src/main/java/com/28minutes/microservices/limits_service/LimitsServiceApplication.java
+++ 6.limits-service/src/main/java/com/28minutes/microservices/limits_service/LimitsServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 	LimitsServiceApplication {
+public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
